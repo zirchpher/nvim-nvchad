c9cb72266f71d0f6a1f570a65fd4760d24b1282c
zirchpher <zirchpher@gmail.com>
2023-06-15 20:35:04 -0500

    Fixed: Colorizer
    Se arregló el problema con tailwinds, los colores no los reconocía

--- configs/colorizer.lua
+++ configs/colorizer.lua
@@ -4,7 +4,6 @@ if not status then
 end
 
 colorizer.setup {
-  -- filetypes = { "*" },
   user_default_options = {
     RGB = true, -- #RGB hex codes
     RRGGBB = true, -- #RRGGBB hex codes
@@ -14,16 +13,19 @@ colorizer.setup {
     rgb_fn = true, -- CSS rgb() and rgba() functions
     hsl_fn = true, -- CSS hsl() and hsla() functions
     css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-    css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+    -- Nota: si activas css_fn no funciona los colores con tailwind
+    css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
     -- Available modes for `mode`: foreground, background,  virtualtext
     mode = "background", -- Set the display mode.
     -- Available methods are false / true / "normal" / "lsp" / "both"
     -- True is same as normal
+    tailwind = true, -- Enable tailwind colors
     -- parsers can contain values used in |user_default_options|
-    sass = { enable = "lsp", parsers = { "css" } }, -- Enable sass colors
+    sass = { enable = false, parsers = { "css" } }, -- Enable sass colors
     virtualtext = "■",
+    -- update color values even if buffer is not focused
+    -- example use: cmp_menu, cmp_docs
     always_update = false,
-    tailwind = true,
   },
   filetypes = {
     "aspnetcorerazor",
