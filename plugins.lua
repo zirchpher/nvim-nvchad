6812ef936a0a56d6015cb6075a2986ecfa10e98d
zirchpher <zirchpher@gmail.com>
2023-08-08 09:42:25 -0500

    pass token chatgpt cambiado

--- plugins.lua
+++ plugins.lua
@@ -134,7 +134,7 @@ local plugins = {
     },
     config = function()
       require("chatgpt").setup {
-        async_api_key_cmd = "pass show api/tokens/openai",
+        async_api_key_cmd = "pass show api/openai",
       }
     end,
   },
